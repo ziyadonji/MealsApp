62e5be807ebc1250563df52693fe226c566410df
ziyadonji <ziyadonji@gmail.com>
2020-08-03 23:04:30 +0530

    initial setup

---
+++ MealsApp/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fudi',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Fudi'),
+      ),
+      body: Center(
+        child: Text("first page"),
+      ),
+    );
+  }
+}
