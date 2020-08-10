2f5aa139686fa4f5928fe858fa51f564293026d4
ziyadonji <ziyadonji@gmail.com>
2020-08-11 00:28:50 +0530

    favourite screen modified

--- MealsApp/lib/screens/favouritesScreen.dart
+++ MealsApp/lib/screens/favouritesScreen.dart
@@ -3,34 +3,33 @@ import 'package:flutter/material.dart';
 import '../widgets/meal_item.dart';
 
 class FavouritesScreen extends StatelessWidget {
-  
   final List<MealDetails> favouriteMeals;
-  FavouritesScreen(this.favouriteMeals,);
-  
+  FavouritesScreen(
+    this.favouriteMeals,
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+body: favouriteMeals.isEmpty?
 
-// body: Center(child: Text(
-//       "favourite screen"
-//     ),),
+Center(child: Text(
+      "favourite screen"
+    ),)
 
-body:ListView.builder(
-          itemBuilder: (context, index) {
-            return MealItem(
-              title: favouriteMeals[index].title,
-              duration: favouriteMeals[index].duration,
-              complexity: favouriteMeals[index].complexity,
-              imageUrl:favouriteMeals[index].imageUrl,
-              affordability: favouriteMeals[index].affordability,
-              id: favouriteMeals[index].id,
-            );
-          },
-          itemCount: favouriteMeals.length,
-        )
-
-
+      : ListView.builder(
+        itemBuilder: (context, index) {
+          return MealItem(
+            title: favouriteMeals[index].title,
+            duration: favouriteMeals[index].duration,
+            complexity: favouriteMeals[index].complexity,
+            imageUrl: favouriteMeals[index].imageUrl,
+            affordability: favouriteMeals[index].affordability,
+            id: favouriteMeals[index].id,
+          );
+        },
+        itemCount: favouriteMeals.length,
+      ),
     );
-
-}}
+  }
+}
